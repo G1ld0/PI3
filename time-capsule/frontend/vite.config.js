67b497ee92eb146fcf62ea922ad35db0c25e9b0c
Gildo Ferreira Dias Filho <123710338+G1ld0@users.noreply.github.com>
2025-04-09 18:01:26 +0000

    ajustando configurações para deploy do frontend no vercel

--- time-capsule/frontend/vite.config.js
+++ time-capsule/frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://time-capsule-backend-m6ul.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
